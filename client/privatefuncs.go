2080854e8584784ae16371e1c0f6373e384e03e3
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-05-28 02:21:46 -0400

    added comment

--- client/privatefuncs.go
+++ client/privatefuncs.go
@@ -9,6 +9,9 @@ import (
 
 // function designed to add metadata containing a filename
 // header to the given context.
+//
+// this returns an outgoing context with the generated
+// metadata attached to it.
 func addFilenameMD(ctx context.Context, filename string) (fctx context.Context, err error) {
 	var md metadata.MD = make(metadata.MD)
 	md.Set(ofscommon.HEADER_FILENAME, filename)
