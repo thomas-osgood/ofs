658a5e272819ed8fa00cb2b3d9fc564db5cf9815
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-05-28 02:21:06 -0400

    added comment

--- client/privatefuncs.go
+++ client/privatefuncs.go
@@ -7,6 +7,8 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
+// function designed to add metadata containing a filename
+// header to the given context.
 func addFilenameMD(ctx context.Context, filename string) (fctx context.Context, err error) {
 	var md metadata.MD = make(metadata.MD)
 	md.Set(ofscommon.HEADER_FILENAME, filename)
