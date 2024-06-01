424a77ceea1a616299d4e762f08eeab5ef7c6960
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-01 17:47:59 -0400

    added new message

--- client/internal/messages/messages.go
+++ client/internal/messages/messages.go
@@ -1,6 +1,7 @@
 package messages
 
 const ERR_EMPTY_ADDR string = "address must be non-empty string"
+const ERR_FILENAME_EMPTY string = "filename cannot be an empty string"
 const ERR_NEGATIVE_TIMEOUT string = "timeout must be a positive second value"
 const ERR_OPEN_FILE string = "[OPENFILE] %s\n"
 const ERR_TRANSMIT_FILE string = "error transmitting file: %s"
