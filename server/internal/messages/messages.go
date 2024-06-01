07d9edb63f06021aa5f11e8b655d712496446d3c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-01 19:06:27 -0400

    added FILE_SENT message

--- server/internal/messages/messages.go
+++ server/internal/messages/messages.go
@@ -26,6 +26,8 @@ const ERR_RECV string = "[RECV] %s"
 const ERR_SERVE string = "[SERVE] %s\n"
 const ERR_TEMP string = "[TEMP] %s"
 
+const FILE_SENT string = "file successfully transmitted"
+
 const SERVER_LISTEN_INFO string = "server listening on \"%s\"\n"
 
 const TEMP_REMOVED string = "temp file removed"
