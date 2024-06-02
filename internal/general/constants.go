2d3da71433bd65b6da8483faf0dce0556b09055c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-02 18:04:45 -0400

    added HEADER_LASTMODIFIED

--- internal/general/constants.go
+++ internal/general/constants.go
@@ -2,5 +2,7 @@ package general
 
 const DEFAULT_CHUNKSIZE int = 2048
 
+const HEADER_LASTMODIFIED string = "x-last-modified"
+
 const HEADER_FILENAME string = "x-filename"
 const HEADER_FILESIZE string = "x-filesize"
