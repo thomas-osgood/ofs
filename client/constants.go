7a77defd15e68a6769dc0ba4cdf3cf2fdc49b089
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-12 15:48:45 -0400

    added DEFAULT_TRANSFER_TIMEOUT

--- client/constants.go
+++ client/constants.go
@@ -4,3 +4,4 @@ import "time"
 
 const DEFAULT_SRVADDR string = "127.0.0.1:8999"
 const DEFAULT_TIMEOUT time.Duration = 30 * time.Second
+const DEFAULT_TRANSFER_TIMEOUT time.Duration = 20 * time.Minute
