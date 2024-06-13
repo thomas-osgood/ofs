a9f1a6777d00db215f05072a994677e017c603bd
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-12 23:25:16 -0400

    added transferCfg field

--- client/structs.go
+++ client/structs.go
@@ -3,24 +3,24 @@ package client
 import (
 	"time"
 
+	"github.com/thomas-osgood/ofs/client/internal/ofctypes"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 type FClient struct {
-	srvaddr         string
-	srvopts         []grpc.DialOption
-	timeout         time.Duration
-	transferTimeout time.Duration
+	srvaddr     string
+	srvopts     []grpc.DialOption
+	timeout     time.Duration
+	transferCfg ofctypes.TransferConfig
 }
 
 type FClientOption struct {
-	Creds   credentials.TransportCredentials
-	Srvaddr string
-	Srvopts []grpc.DialOption
+	Creds       credentials.TransportCredentials
+	Srvaddr     string
+	Srvopts     []grpc.DialOption
+	TransferCfg ofctypes.TransferConfig
 	// timeout value for making connections to the server. this will
 	// be applied to both upload and download functions.
 	Timeout time.Duration
-	// timeout value for downloading and uploading files.
-	TransferTimeout time.Duration
 }
