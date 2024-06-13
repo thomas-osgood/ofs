b7a123f7844466f7e3702de2a62904cde5821c87
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-12 23:24:11 -0400

    added DEFAULT_MAX_DOWNLOADS, DEFAULT_MAX_UPLOADS

--- client/constants.go
+++ client/constants.go
@@ -1,7 +1,11 @@
 package client
 
-import "time"
+import (
+	"time"
+)
 
+const DEFAULT_MAX_DOWNLOADS int = 10
+const DEFAULT_MAX_UPLOADS int = 10
 const DEFAULT_SRVADDR string = "127.0.0.1:8999"
 const DEFAULT_TIMEOUT time.Duration = 30 * time.Second
 const DEFAULT_TRANSFER_TIMEOUT time.Duration = 20 * time.Minute
