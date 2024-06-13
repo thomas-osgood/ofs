9f9c05c9af926cd7236cda4b82fd3026f9deb476
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-13 19:49:23 -0400

    added comment

--- client/functions.go
+++ client/functions.go
@@ -37,6 +37,11 @@ func NewClient(opts ...FClientOptFunc) (client *FClient, err error) {
 		}
 	}
 
+	// build channels to create semaphore objects.
+	//
+	// reference:
+	//
+	// https://medium.com/@deckarep/gos-extended-concurrency-semaphores-part-1-5eeabfa351ce
 	defaults.TransferCfg.DownSem = make(chan struct{}, defaults.TransferCfg.MaxDownloads)
 	defaults.TransferCfg.UpSem = make(chan struct{}, defaults.TransferCfg.MaxUploads)
 
