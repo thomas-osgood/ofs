4d50749ed0dd49d51c366505c5232a79bcf77cf4
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-15 17:49:26 -0400

    renamed common to protocommon

--- server/server.go
+++ server/server.go
@@ -12,7 +12,7 @@ import (
 	"time"
 
 	ofscommon "github.com/thomas-osgood/ofs/internal/general"
-	"github.com/thomas-osgood/ofs/protobufs/common"
+	protocommon "github.com/thomas-osgood/ofs/protobufs/common"
 	"github.com/thomas-osgood/ofs/protobufs/filehandler"
 	"github.com/thomas-osgood/ofs/protobufs/pingpong"
 	ofsdefaults "github.com/thomas-osgood/ofs/server/internal/defaults"
@@ -25,10 +25,10 @@ import (
 
 // function designed to delete a file in the uploads directory
 // as requested by the client.
-func (fsrv *FServer) DeleteFile(ctx context.Context, req *filehandler.FileRequest) (resp *common.StatusMessage, err error) {
+func (fsrv *FServer) DeleteFile(ctx context.Context, req *filehandler.FileRequest) (resp *protocommon.StatusMessage, err error) {
 	var targetpath string = strings.TrimSpace(req.GetFilename())
 
-	resp = &common.StatusMessage{
+	resp = &protocommon.StatusMessage{
 		Code:    http.StatusOK,
 		Message: ofsmessages.FILE_DELETED,
 	}
@@ -108,7 +108,7 @@ func (fsrv *FServer) DownloadFile(srv filehandler.Fileservice_DownloadFileServer
 // the ability to download files from. if there are separate upload and
 // download directories, only the "uploads" (files that can be uploaded
 // from the server to client) directory will be listed.
-func (fsrv *FServer) ListFiles(mpty *common.Empty, srv filehandler.Fileservice_ListFilesServer) (err error) {
+func (fsrv *FServer) ListFiles(mpty *protocommon.Empty, srv filehandler.Fileservice_ListFilesServer) (err error) {
 	var curfile *filehandler.FileInfo
 	var files []*filehandler.FileInfo
 
@@ -136,12 +136,12 @@ func (fsrv *FServer) ListFiles(mpty *common.Empty, srv filehandler.Fileservice_L
 // successful code: 201 Created
 //
 // failure code: 500 Internal Server Error
-func (fsrv *FServer) MakeDirectory(ctx context.Context, dirreq *filehandler.MakeDirectoryRequest) (retstatus *common.StatusMessage, err error) {
+func (fsrv *FServer) MakeDirectory(ctx context.Context, dirreq *filehandler.MakeDirectoryRequest) (retstatus *protocommon.StatusMessage, err error) {
 	var subdir string = fsrv.buildUploadFilename(dirreq.GetDirname())
 
 	// initialize the successful StatusMessage. if everything
 	// works as expected, this will not be modified.
-	retstatus = &common.StatusMessage{
+	retstatus = &protocommon.StatusMessage{
 		Code:    http.StatusCreated,
 		Message: ofsmessages.DIR_CREATED,
 	}
@@ -180,11 +180,11 @@ func (fsrv *FServer) Ping(ctx context.Context, ping *pingpong.Ping) (pong *pingp
 //
 // note: if the destination file already exists, or if there is an issue
 // related to permissions, an error will be returned.
-func (fsrv *FServer) RenameFile(ctx context.Context, rnreq *filehandler.RenameFileRequest) (resp *common.StatusMessage, err error) {
+func (fsrv *FServer) RenameFile(ctx context.Context, rnreq *filehandler.RenameFileRequest) (resp *protocommon.StatusMessage, err error) {
 	var absdest string = fsrv.buildUploadFilename(rnreq.GetNewfilename())
 	var abssrc string = fsrv.buildUploadFilename(rnreq.GetOldfilename())
 
-	resp = new(common.StatusMessage)
+	resp = new(protocommon.StatusMessage)
 
 	// check for the existence of the destination file. if the
 	// destination file already exists, an error will be returned
@@ -211,7 +211,7 @@ func (fsrv *FServer) RenameFile(ctx context.Context, rnreq *filehandler.RenameFi
 
 // function designed to calculate and return the amount of storage (in bytes)
 // consumed by the uploads and downloads directories.
-func (fsrv *FServer) StorageBreakdown(ctx context.Context, mpty *common.Empty) (consumption *filehandler.StorageInfo, err error) {
+func (fsrv *FServer) StorageBreakdown(ctx context.Context, mpty *protocommon.Empty) (consumption *filehandler.StorageInfo, err error) {
 	consumption = new(filehandler.StorageInfo)
 
 	consumption.Downloads, err = fsrv.calculateDirectoryConsumption(fsrv.downloadsdir)
