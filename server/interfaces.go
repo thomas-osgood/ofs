ee7c68743d5473468f36615ff978c95406899c5c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-16 22:35:25 -0400

    began defining OFSEncryptor interface

---
+++ server/interfaces.go
@@ -0,0 +1,14 @@
+package server
+
+// interface defining an object that can perform various
+// encryption methods.
+//
+// this can be used to encrypt and decrypt files on the server.
+type OFSEncryptor interface {
+	// decrypt a given encrypted file.
+	DecryptFile(string) error
+	// encrypt a given file.
+	EncryptFile(string) error
+	// autogenerate and return an encryption key.
+	GenerateKey() ([]byte, error)
+}
