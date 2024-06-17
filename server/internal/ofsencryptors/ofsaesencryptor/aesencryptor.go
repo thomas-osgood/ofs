6412bb506484589e3d43365806b3a6e5aa79ac87
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 18:43:01 -0400

    added logic to EncryptFile func

--- server/internal/ofsencryptors/ofsaesencryptor/aesencryptor.go
+++ server/internal/ofsencryptors/ofsaesencryptor/aesencryptor.go
@@ -1,5 +1,14 @@
 package ofsaesencryptor
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"io"
+
+	ofscommon "github.com/thomas-osgood/ofs/internal/general"
+)
+
 // function designed to decrypt a file using AES encryption.
 func (ae *AESEncryptor) DecryptFile(filename string) (err error) {
 	return nil
@@ -7,7 +16,33 @@ func (ae *AESEncryptor) DecryptFile(filename string) (err error) {
 
 // function designed to encrypt a file using AES encryption.
 func (ae *AESEncryptor) EncryptFile(filename string) (err error) {
-	return nil
+	var aesblock cipher.Block
+	var ciphertext []byte
+	var gcm cipher.AEAD
+	var nonce []byte
+	var plaintext []byte
+
+	plaintext, err = ofscommon.ReadFileBytes(filename)
+	if err != nil {
+		return err
+	}
+
+	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+		return err
+	}
+
+	if gcm, err = cipher.NewGCM(aesblock); err != nil {
+		return err
+	}
+	nonce = make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return err
+	}
+
+	ciphertext = gcm.Seal(nonce, nonce, plaintext, nil)
+
+	return ofscommon.WriteFileBytes(filename, ciphertext)
 }
 
 // function designed to encrypt a file using AES encryption.
