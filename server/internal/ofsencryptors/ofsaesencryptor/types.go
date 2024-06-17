9482c537db6c696b8dbfa89f8b14940a5b162a0e
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 02:16:03 -0400

    added new fields for AESEncryptor

--- server/internal/ofsencryptors/ofsaesencryptor/types.go
+++ server/internal/ofsencryptors/ofsaesencryptor/types.go
@@ -2,6 +2,11 @@ package ofsaesencryptor
 
 type AESEncryptor struct {
 	key []byte
+
+	// private key for AES encryption.
+	privkey []byte
+	// public key for AES encryption.
+	pubkey []byte
 }
 
 type AESEncryptorOpt struct {
