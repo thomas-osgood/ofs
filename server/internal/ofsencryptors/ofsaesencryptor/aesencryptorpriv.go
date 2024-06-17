9aec844f8c53101cd6d073718302163c444704ba
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 19:15:47 -0400

    added decryptBytesAES

---
+++ server/internal/ofsencryptors/ofsaesencryptor/aesencryptorpriv.go
@@ -0,0 +1,38 @@
+package ofsaesencryptor
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+
+	ofsmessages "github.com/thomas-osgood/ofs/server/internal/messages"
+)
+
+// function designed to decrypt bytes using a given AES key.
+func (ae *AESEncryptor) decryptBytesAES(ciphertext []byte) (plaintext []byte, err error) {
+	var aesblock cipher.Block
+	var gcm cipher.AEAD
+	var nonce []byte
+	var nonceSize int
+
+	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = cipher.NewGCM(aesblock); err != nil {
+		return nil, err
+	}
+
+	nonceSize = gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, fmt.Errorf(ofsmessages.ERR_CIPHERTEXT_NONCE_SIZE)
+	}
+
+	nonce, ciphertext = ciphertext[:nonceSize], ciphertext[nonceSize:]
+
+	if plaintext, err = gcm.Open(nil, nonce, ciphertext, nil); err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
