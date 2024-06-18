74785b0ca477c1ee22a121b63ffcccce8a39ef67
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:00:30 -0400

    removed unnecessary fields

--- server/internal/ofsencryptors/ofsaesencryptor/types.go
+++ server/internal/ofsencryptors/ofsaesencryptor/types.go
@@ -2,11 +2,6 @@ package ofsaesencryptor
 
 type AESEncryptor struct {
 	key []byte
-
-	// private key for AES encryption.
-	privkey []byte
-	// public key for AES encryption.
-	pubkey []byte
 }
 
 type AESEncryptorOpt struct {
