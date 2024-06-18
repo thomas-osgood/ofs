3d50b90f3a547e545b578c9affded61a7397655e
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:37:32 -0400

    reduced logic side of encrypt/decrypt file funcs

--- ofsencryptors/ofsaesencryptor/aesencryptor.go
+++ ofsencryptors/ofsaesencryptor/aesencryptor.go
@@ -1,41 +1,15 @@
 package ofsaesencryptor
 
 import (
-	ofscommon "github.com/thomas-osgood/ofs/internal/general"
+	consts "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/constants"
 )
 
 // function designed to decrypt a file using AES encryption.
 func (ae *AESEncryptor) DecryptFile(filename string) (err error) {
-	var ciphertext []byte
-	var plaintext []byte
-
-	ciphertext, err = ofscommon.ReadFileBytes(filename)
-	if err != nil {
-		return err
-	}
-
-	plaintext, err = ae.decryptBytesAES(ciphertext)
-	if err != nil {
-		return err
-	}
-
-	return ofscommon.WriteFileBytes(filename, plaintext)
+	return ae.manipulateFileData(filename, consts.ACT_DECRYPT)
 }
 
 // function designed to encrypt a file using AES encryption.
 func (ae *AESEncryptor) EncryptFile(filename string) (err error) {
-	var ciphertext []byte
-	var plaintext []byte
-
-	plaintext, err = ofscommon.ReadFileBytes(filename)
-	if err != nil {
-		return err
-	}
-
-	ciphertext, err = ae.encryptBytesAES(plaintext)
-	if err != nil {
-		return err
-	}
-
-	return ofscommon.WriteFileBytes(filename, ciphertext)
+	return ae.manipulateFileData(filename, consts.ACT_ENCRYPT)
 }
