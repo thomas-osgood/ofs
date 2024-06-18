c9b5f596c414220cfde578db09f27bbae24b5436
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:18:45 -0400

    use AES constants in validation

--- ofsencryptors/ofsaesencryptor/internal/validations/validations.go
+++ ofsencryptors/ofsaesencryptor/internal/validations/validations.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	encryptormessages "github.com/thomas-osgood/ofs/ofsencryptors/internal/messages"
+	consts "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/constants"
 )
 
 // function designed to validate an AES encryptor's key.
 func ValidateKey(key []byte) (err error) {
 	var keylen int = len(key)
 
-	if (keylen != 32) && (keylen != 24) && (keylen != 16) {
+	if (keylen != consts.AES_128_KEYLENGTH) && (keylen != consts.AES_192_KEYLENGTH) && (keylen != consts.AES_256_KEYLENGTH) {
 		return fmt.Errorf(encryptormessages.ERR_AES_KEYLEN)
 	}
 
