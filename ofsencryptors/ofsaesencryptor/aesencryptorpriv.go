2950b1ed1e91ee4197ee4d203a124c6c703e9e13
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:26:05 -0400

    added encryptBytesAES func

--- ofsencryptors/ofsaesencryptor/aesencryptorpriv.go
+++ ofsencryptors/ofsaesencryptor/aesencryptorpriv.go
@@ -3,12 +3,14 @@ package ofsaesencryptor
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"fmt"
+	"io"
 
 	encryptormessages "github.com/thomas-osgood/ofs/ofsencryptors/internal/messages"
 )
 
-// function designed to decrypt bytes using a given AES key.
+// function designed to decrypt bytes and return the "plainbytes".
 func (ae *AESEncryptor) decryptBytesAES(ciphertext []byte) (plaintext []byte, err error) {
 	var aesblock cipher.Block
 	var gcm cipher.AEAD
@@ -35,4 +37,28 @@ func (ae *AESEncryptor) decryptBytesAES(ciphertext []byte) (plaintext []byte, er
 	}
 
 	return plaintext, nil
+}
+
+// function designed to encrypt bytes and return the "cipherbytes".
+func (ae *AESEncryptor) encryptBytesAES(plaintext []byte) (ciphertext []byte, err error) {
+	var aesblock cipher.Block
+	var gcm cipher.AEAD
+	var nonce []byte
+
+	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = cipher.NewGCM(aesblock); err != nil {
+		return nil, err
+	}
+	nonce = make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext = gcm.Seal(nonce, nonce, plaintext, nil)
+
+	return ciphertext, nil
 }
