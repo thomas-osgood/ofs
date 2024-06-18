4c32d259caaf62e9aa742354e6bb80524d447710
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:26:17 -0400

    implemented encryptBytesAES func

--- ofsencryptors/ofsaesencryptor/aesencryptor.go
+++ ofsencryptors/ofsaesencryptor/aesencryptor.go
@@ -1,11 +1,6 @@
 package ofsaesencryptor
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"io"
-
 	ofscommon "github.com/thomas-osgood/ofs/internal/general"
 )
 
@@ -29,10 +24,7 @@ func (ae *AESEncryptor) DecryptFile(filename string) (err error) {
 
 // function designed to encrypt a file using AES encryption.
 func (ae *AESEncryptor) EncryptFile(filename string) (err error) {
-	var aesblock cipher.Block
 	var ciphertext []byte
-	var gcm cipher.AEAD
-	var nonce []byte
 	var plaintext []byte
 
 	plaintext, err = ofscommon.ReadFileBytes(filename)
@@ -40,20 +32,10 @@ func (ae *AESEncryptor) EncryptFile(filename string) (err error) {
 		return err
 	}
 
-	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+	ciphertext, err = ae.encryptBytesAES(plaintext)
+	if err != nil {
 		return err
 	}
-
-	if gcm, err = cipher.NewGCM(aesblock); err != nil {
-		return err
-	}
-	nonce = make([]byte, gcm.NonceSize())
-
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return err
-	}
-
-	ciphertext = gcm.Seal(nonce, nonce, plaintext, nil)
 
 	return ofscommon.WriteFileBytes(filename, ciphertext)
 }
