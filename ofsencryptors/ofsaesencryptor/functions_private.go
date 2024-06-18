1cf41061c18c3d0a61d7bcad40942caaef9baa3f
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-18 19:32:09 -0400

    implemented GenerateRandomBytes in autogenKey

--- ofsencryptors/ofsaesencryptor/functions_private.go
+++ ofsencryptors/ofsaesencryptor/functions_private.go
@@ -3,6 +3,8 @@ package ofsaesencryptor
 import (
 	"fmt"
 
+	ofscommon "github.com/thomas-osgood/ofs/internal/general"
+	consts "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/constants"
 	encmsg "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/messages"
 )
 
@@ -13,11 +15,12 @@ import (
 func autogenKey(keytype int) (key []byte, err error) {
 	switch keytype {
 	case AES_128:
+		return ofscommon.GenerateRandomBytes(consts.AES_128_KEYLENGTH, consts.AES_128_KEYLENGTH+1)
 	case AES_192:
+		return ofscommon.GenerateRandomBytes(consts.AES_192_KEYLENGTH, consts.AES_192_KEYLENGTH+1)
 	case AES_256:
+		return ofscommon.GenerateRandomBytes(consts.AES_256_KEYLENGTH, consts.AES_256_KEYLENGTH+1)
 	default:
 		return nil, fmt.Errorf(encmsg.ERR_KEYTYP)
 	}
-
-	return key, nil
 }
