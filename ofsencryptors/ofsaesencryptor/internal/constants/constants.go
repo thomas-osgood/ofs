c4f077969a901345a5117c018907ba3fa2aefea1
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-17 23:36:54 -0400

    added enums and error message

--- ofsencryptors/ofsaesencryptor/internal/constants/constants.go
+++ ofsencryptors/ofsaesencryptor/internal/constants/constants.go
@@ -1,5 +1,13 @@
 package constants
 
+// enum defining possible actions for the encryptor.
+const (
+	ACT_DECRYPT = iota
+	ACT_ENCRYPT
+)
+
 const AES_128_KEYLENGTH int = 16
 const AES_192_KEYLENGTH int = 24
 const AES_256_KEYLENGTH int = 32
+
+const ERR_ACTION_UNKNOWN string = "unknown action specified"
