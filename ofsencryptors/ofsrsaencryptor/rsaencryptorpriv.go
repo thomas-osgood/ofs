f92bb67dbd85722de0cffde1b7b6f02c123c879b
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-18 22:54:20 -0400

    skeleton of manipulateFileData func

---
+++ ofsencryptors/ofsrsaencryptor/rsaencryptorpriv.go
@@ -0,0 +1,8 @@
+package ofsrsaencryptor
+
+// func designed to manipulate the data contained within
+// a given file. this will either encrypt or decrypt the
+// data and write it back to the file.
+func (rsae *RSAEncryptor) manipulateFileData(filename string, action int) (err error) {
+	return nil
+}
