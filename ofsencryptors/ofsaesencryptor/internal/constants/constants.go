d0cd78447b3b2a6fb84fdc3aa569f5fda66ea36f
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-18 22:51:42 -0400

    moved action enum to ofsencryptors internal

--- ofsencryptors/ofsaesencryptor/internal/constants/constants.go
+++ ofsencryptors/ofsaesencryptor/internal/constants/constants.go
@@ -1,11 +1,5 @@
 package constants
 
-// enum defining possible actions for the encryptor.
-const (
-	ACT_DECRYPT = iota
-	ACT_ENCRYPT
-)
-
 const AES_128_KEYLENGTH int = 16
 const AES_192_KEYLENGTH int = 24
 const AES_256_KEYLENGTH int = 32
