51bc3e5e7479c7adbf5589a397a7941a2b97e2b2
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-19 02:32:06 -0400

    added to module comment.

--- ofsencryptors/ofsrsaencryptor/rsaencryptor.go
+++ ofsencryptors/ofsrsaencryptor/rsaencryptor.go
@@ -1,6 +1,12 @@
 // module defining an RSA Encryptor object. this encryptor
 // is able to be used with OFS (or as a stand-alone object)
 // to encrypt and decrypt files.
+//
+// note: this encryptor should only be used if the files that
+// will be saved, served and encrypted are small. the max file
+// size that can be encrypted is ((keysize / 8) - headersize).
+// for a key of 4096 bits using PKCS1 that means ((4096 / 8) - 11)
+// which comes out to a max file size of 501 bytes.
 package ofsrsaencryptor
 
 import (
