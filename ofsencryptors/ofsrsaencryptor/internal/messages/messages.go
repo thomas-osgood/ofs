84b711e949424f3f655fa700099cf5df5f9297b4
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-20 03:25:43 -0400

    added ERR_PUBKEY_DEC and ERR_PUBKEY_GEN

--- ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
+++ ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
@@ -3,3 +3,5 @@ package messages
 const ERR_CONTENT_TOO_LONG string = "content exceeds max encryption length"
 const ERR_PRIVKEY_DEC string = "unable to decode private key to pem"
 const ERR_PRIVKEY_GEN string = "unable to generate private key from bytes"
+const ERR_PUBKEY_DEC string = "unable to decode public key to pem"
+const ERR_PUBKEY_GEN string = "unable to generate public key from bytes"
