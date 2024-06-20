ca3251be5cbb28e56b0430d568d90703de48e1f6
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-20 03:35:00 -0400

    made encrypt/decrypt bytes funcs public

--- ofsencryptors/ofsaesencryptor/aesencryptor.go
+++ ofsencryptors/ofsaesencryptor/aesencryptor.go
@@ -4,12 +4,72 @@
 package ofsaesencryptor
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+
 	consts "github.com/thomas-osgood/ofs/ofsencryptors/internal/constants"
+	aesmessages "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/messages"
 )
+
+// function designed to decrypt bytes and return the "plainbytes".
+func (ae *AESEncryptor) DecryptBytes(ciphertext []byte) (plaintext []byte, err error) {
+	var aesblock cipher.Block
+	var gcm cipher.AEAD
+	var nonce []byte
+	var nonceSize int
+
+	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = cipher.NewGCM(aesblock); err != nil {
+		return nil, err
+	}
+
+	nonceSize = gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		return nil, fmt.Errorf(aesmessages.ERR_CIPHERTEXT_NONCE_SIZE)
+	}
+
+	nonce, ciphertext = ciphertext[:nonceSize], ciphertext[nonceSize:]
+
+	if plaintext, err = gcm.Open(nil, nonce, ciphertext, nil); err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
 
 // function designed to decrypt a file using AES encryption.
 func (ae *AESEncryptor) DecryptFile(filename string) (err error) {
 	return ae.manipulateFileData(filename, consts.ACT_DECRYPT)
+}
+
+// function designed to encrypt bytes and return the "cipherbytes".
+func (ae *AESEncryptor) EncryptBytes(plaintext []byte) (ciphertext []byte, err error) {
+	var aesblock cipher.Block
+	var gcm cipher.AEAD
+	var nonce []byte
+
+	if aesblock, err = aes.NewCipher(ae.key); err != nil {
+		return nil, err
+	}
+
+	if gcm, err = cipher.NewGCM(aesblock); err != nil {
+		return nil, err
+	}
+	nonce = make([]byte, gcm.NonceSize())
+
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+
+	ciphertext = gcm.Seal(nonce, nonce, plaintext, nil)
+
+	return ciphertext, nil
 }
 
 // function designed to encrypt a file using AES encryption.
--- ofsencryptors/ofsaesencryptor/aesencryptorpriv.go
+++ ofsencryptors/ofsaesencryptor/aesencryptorpriv.go
@@ -1,70 +1,12 @@
 package ofsaesencryptor
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"fmt"
-	"io"
 
 	ofscommon "github.com/thomas-osgood/ofs/internal/general"
 	consts "github.com/thomas-osgood/ofs/ofsencryptors/internal/constants"
 	encmessages "github.com/thomas-osgood/ofs/ofsencryptors/internal/messages"
-	aesmessages "github.com/thomas-osgood/ofs/ofsencryptors/ofsaesencryptor/internal/messages"
 )
-
-// function designed to decrypt bytes and return the "plainbytes".
-func (ae *AESEncryptor) decryptBytesAES(ciphertext []byte) (plaintext []byte, err error) {
-	var aesblock cipher.Block
-	var gcm cipher.AEAD
-	var nonce []byte
-	var nonceSize int
-
-	if aesblock, err = aes.NewCipher(ae.key); err != nil {
-		return nil, err
-	}
-
-	if gcm, err = cipher.NewGCM(aesblock); err != nil {
-		return nil, err
-	}
-
-	nonceSize = gcm.NonceSize()
-	if len(ciphertext) < nonceSize {
-		return nil, fmt.Errorf(aesmessages.ERR_CIPHERTEXT_NONCE_SIZE)
-	}
-
-	nonce, ciphertext = ciphertext[:nonceSize], ciphertext[nonceSize:]
-
-	if plaintext, err = gcm.Open(nil, nonce, ciphertext, nil); err != nil {
-		return nil, err
-	}
-
-	return plaintext, nil
-}
-
-// function designed to encrypt bytes and return the "cipherbytes".
-func (ae *AESEncryptor) encryptBytesAES(plaintext []byte) (ciphertext []byte, err error) {
-	var aesblock cipher.Block
-	var gcm cipher.AEAD
-	var nonce []byte
-
-	if aesblock, err = aes.NewCipher(ae.key); err != nil {
-		return nil, err
-	}
-
-	if gcm, err = cipher.NewGCM(aesblock); err != nil {
-		return nil, err
-	}
-	nonce = make([]byte, gcm.NonceSize())
-
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		return nil, err
-	}
-
-	ciphertext = gcm.Seal(nonce, nonce, plaintext, nil)
-
-	return ciphertext, nil
-}
 
 // function designed to encrypt or decrypt a file based on the
 // action specified by the user.
@@ -82,9 +24,9 @@ func (ae *AESEncryptor) manipulateFileData(filename string, action int) (err err
 
 	switch action {
 	case consts.ACT_DECRYPT:
-		output, err = ae.decryptBytesAES(original)
+		output, err = ae.DecryptBytes(original)
 	case consts.ACT_ENCRYPT:
-		output, err = ae.encryptBytesAES(original)
+		output, err = ae.EncryptBytes(original)
 	default:
 		err = fmt.Errorf(encmessages.ERR_ACTION_UNKNOWN)
 	}
