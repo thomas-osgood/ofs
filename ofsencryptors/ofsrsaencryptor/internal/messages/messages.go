3d3c22294faaaf90f082f37b4a42703bd8956be7
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-19 23:19:04 -0400

    added ERR_PRIVKEY_DEC

--- ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
+++ ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
@@ -1,4 +1,5 @@
 package messages
 
 const ERR_CONTENT_TOO_LONG string = "content exceeds max encryption length"
+const ERR_PRIVKEY_DEC string = "unable to decode private key to pem"
 const ERR_PRIVKEY_GEN string = "unable to generate private key from bytes"
