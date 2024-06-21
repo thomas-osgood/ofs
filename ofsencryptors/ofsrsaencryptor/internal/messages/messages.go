aa9d350e076a22f36bd4b00f09ba437a501f33fb
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-20 23:28:17 -0400

    added ERR_KEY_TYPE_UNSUPPORTED

--- ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
+++ ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
@@ -1,6 +1,7 @@
 package messages
 
 const ERR_CONTENT_TOO_LONG string = "content exceeds max encryption length"
+const ERR_KEY_TYPE_UNSUPPORTED string = "\"%s\" key type is not supported"
 const ERR_KEY_TYPE string = "unknown key type \"%s\""
 const ERR_PRIVKEY_DEC string = "unable to decode private key to pem"
 const ERR_PRIVKEY_GEN string = "unable to generate private key from bytes"
