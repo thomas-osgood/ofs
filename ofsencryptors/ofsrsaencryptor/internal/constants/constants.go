41554df9bcf92bb723abafa4a2133fc15d8ac499
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-20 23:21:31 -0400

    added new constants

--- ofsencryptors/ofsrsaencryptor/internal/constants/constants.go
+++ ofsencryptors/ofsrsaencryptor/internal/constants/constants.go
@@ -2,6 +2,10 @@ package constants
 
 const KEYSIZE int = 4096
 
+const KEY_TYPE_CERT string = "CERTIFICATE"
+const KEY_TYPE_PRIVKEY string = "PRIVATE KEY"
+const KEY_TYPE_PUBKEY string = "PUBLIC KEY"
+
 const PKCS_HEADER_LEN int = 11
 
 const RSA_TYPE_PRIVKEY string = "RSA PRIVATE KEY"
