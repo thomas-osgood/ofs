7463ae91725b9467dd9782e49797e13a0c2eea0e
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-20 23:21:36 -0400

    added ERR_KEY_TYPE

--- ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
+++ ofsencryptors/ofsrsaencryptor/internal/messages/messages.go
@@ -1,6 +1,7 @@
 package messages
 
 const ERR_CONTENT_TOO_LONG string = "content exceeds max encryption length"
+const ERR_KEY_TYPE string = "unknown key type \"%s\""
 const ERR_PRIVKEY_DEC string = "unable to decode private key to pem"
 const ERR_PRIVKEY_GEN string = "unable to generate private key from bytes"
 const ERR_PUBKEY_DEC string = "unable to decode public key to pem"
