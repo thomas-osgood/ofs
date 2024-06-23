aa3b539268340fccb299f1bd6c4548f7fd880772
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 00:03:34 -0400

    updated OFSAuthenticator interface

--- ofsauthenticators/interfaces.go
+++ ofsauthenticators/interfaces.go
@@ -2,6 +2,6 @@ package ofsauthenticators
 
 // interface defining an object that can be used to
 // authenticate client requests to the server.
-type OFSAuthenticator[T AuthRequest] interface {
-	ValidateUser(T) error
+type OFSAuthenticator interface {
+	ValidateUser(interface{}) error
 }
