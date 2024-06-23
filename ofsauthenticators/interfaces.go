772c62c710391eab5663038c50c5d0b808c415e0
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 15:47:08 -0400

    updated OFSAuthenticator interface

--- ofsauthenticators/interfaces.go
+++ ofsauthenticators/interfaces.go
@@ -1,7 +1,12 @@
 package ofsauthenticators
 
+import "google.golang.org/grpc/metadata"
+
 // interface defining an object that can be used to
 // authenticate client requests to the server.
 type OFSAuthenticator interface {
-	ValidateUser(interface{}) error
+	// function designed to parse metadata passed into it,
+	// pull out the correct credential headers and verify
+	// whether the given credentials are valid for a user.
+	ValidateUser(metadata.MD) error
 }
