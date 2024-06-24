47067843d3935accb3782c8db6e2bc8bae5c1ab1
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 23:23:22 -0400

    added ERR_DBUSER_BLANK

--- ofsauthenticators/dbauthenticators/internal/messages/messages.go
+++ ofsauthenticators/dbauthenticators/internal/messages/messages.go
@@ -2,8 +2,9 @@ package messages
 
 const DRIVER_POSTGRES string = "postgres"
 
-const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
 const ERR_DBHOST_BLANK string = "database host cannot be an empty string"
+const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
+const ERR_DBUSER_BLANK string = "database user cannot be an empty string"
 const ERR_SCHEMA_BLANK string = "schema name cannto be an empty string"
 const ERR_TIMEOUT string = "db connection timeout"
 const ERR_TIMEOUT_SMALL string = "timeout value must be at least 1 second"
