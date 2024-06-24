f2fe08411f50ee61cb76fc1ee3ebf9a373190012
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 22:31:51 -0400

    began checkConnection func

---
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/authenticator.go
@@ -0,0 +1,9 @@
+package postgresauthenticator
+
+// function designed to ping the database and make sure the
+// connection is valid.
+//
+// if the connection is valid, nil will be returned.
+func (pga *PostGresAuthenticator) checkConnection() (err error) {
+	return nil
+}
