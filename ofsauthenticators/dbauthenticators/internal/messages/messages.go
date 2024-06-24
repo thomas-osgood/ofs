4cbac7de78ed97ccbaaaa9f8f41f734430cde73c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 23:10:23 -0400

    added DRIVER_POSTGRES

--- ofsauthenticators/dbauthenticators/internal/messages/messages.go
+++ ofsauthenticators/dbauthenticators/internal/messages/messages.go
@@ -1,4 +1,6 @@
 package messages
 
+const DRIVER_POSTGRES string = "postgres"
+
 const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
 const ERR_SCHEMA_BLANK string = "schema name cannto be an empty string"
