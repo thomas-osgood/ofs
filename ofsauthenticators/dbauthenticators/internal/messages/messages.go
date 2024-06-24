45cac473142e988969599515817b3c3e4a1314e4
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 23:24:31 -0400

    added ERR_DBPASS_BLANK

--- ofsauthenticators/dbauthenticators/internal/messages/messages.go
+++ ofsauthenticators/dbauthenticators/internal/messages/messages.go
@@ -4,6 +4,7 @@ const DRIVER_POSTGRES string = "postgres"
 
 const ERR_DBHOST_BLANK string = "database host cannot be an empty string"
 const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
+const ERR_DBPASS_BLANK string = "database password cannot be an empty string"
 const ERR_DBUSER_BLANK string = "database user cannot be an empty string"
 const ERR_SCHEMA_BLANK string = "schema name cannto be an empty string"
 const ERR_TIMEOUT string = "db connection timeout"
