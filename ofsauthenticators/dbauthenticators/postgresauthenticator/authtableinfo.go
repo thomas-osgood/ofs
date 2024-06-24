ac697edd6cea64f442857ce03a74d5d836c8b95e
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-24 02:27:49 -0400

    added IsNil func

---
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/authtableinfo.go
@@ -0,0 +1,21 @@
+package postgresauthenticator
+
+// function designed to determine if the AuthTableInfo
+// struct is nil. this will be set to true if all fields
+// are empty strings.
+func (ati *AuthTableInfo) IsNil() bool {
+	var curcol string
+	var fields []string
+
+	ati.cleanStrings()
+
+	fields = []string{ati.Tablename, ati.Usercolumn, ati.Passcolumn}
+
+	for _, curcol = range fields {
+		if len(curcol) > 0 {
+			return false
+		}
+	}
+
+	return true
+}
