82be43250d240ce3bab5edb629bab8c4033a5dae
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-24 02:38:58 -0400

    added SSL consts

--- ofsauthenticators/dbauthenticators/postgresauthenticator/internal/constants/constants.go
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/internal/constants/constants.go
@@ -1,3 +1,6 @@
 package constants
 
 const CONNECTION_STRING string = "host=%s port=%d user=%s password=%s dbname=%s sslmode=%s"
+
+const SSL_DISABLE string = "disable"
+const SSL_ENABLE string = "enable"
