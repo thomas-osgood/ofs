5c37f06c3dc2af5947ccbf40be43b5917040a572
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 23:18:03 -0400

    added ERR_TIMEOUT

--- ofsauthenticators/dbauthenticators/internal/messages/messages.go
+++ ofsauthenticators/dbauthenticators/internal/messages/messages.go
@@ -4,3 +4,4 @@ const DRIVER_POSTGRES string = "postgres"
 
 const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
 const ERR_SCHEMA_BLANK string = "schema name cannto be an empty string"
+const ERR_TIMEOUT string = "db connection timeout"
