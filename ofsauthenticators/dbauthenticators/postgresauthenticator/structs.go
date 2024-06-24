0e793627988ec2d1dc5c64837a89725308498300
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-24 18:39:02 -0400

    added hasher field

--- ofsauthenticators/dbauthenticators/postgresauthenticator/structs.go
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/structs.go
@@ -4,17 +4,20 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/thomas-osgood/ofs/ofsauthenticators"
 	"github.com/thomas-osgood/ofs/ofsauthenticators/dbauthenticators"
 )
 
 type PostGresAuthenticator struct {
 	db        *sql.DB
+	hasher    ofsauthenticators.Hasher
 	tableinfo dbauthenticators.AuthTableInfo
 	timeout   time.Duration
 }
 
 type PostGresAuthOption struct {
 	Dbname    string
+	Hasher    ofsauthenticators.Hasher
 	Host      string
 	Password  string
 	Port      int
