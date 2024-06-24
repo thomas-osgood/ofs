5038fee69f0cab3df9546d9aca0d7c45076e6369
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-23 23:21:42 -0400

    added ERR_DBHOST_BLANK

--- ofsauthenticators/dbauthenticators/internal/messages/messages.go
+++ ofsauthenticators/dbauthenticators/internal/messages/messages.go
@@ -3,6 +3,7 @@ package messages
 const DRIVER_POSTGRES string = "postgres"
 
 const ERR_DBNAME_BLANK string = "database name cannot be an empty string"
+const ERR_DBHOST_BLANK string = "database host cannot be an empty string"
 const ERR_SCHEMA_BLANK string = "schema name cannto be an empty string"
 const ERR_TIMEOUT string = "db connection timeout"
 const ERR_TIMEOUT_SMALL string = "timeout value must be at least 1 second"
