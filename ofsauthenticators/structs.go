88fdd1f93dd9bd2c3a5e5c214ac47751851ea5e9
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-25 02:48:53 -0400

    added MetadataInfo struct

--- ofsauthenticators/structs.go
+++ ofsauthenticators/structs.go
@@ -10,3 +10,12 @@ type UserPassRequest struct {
 	Username string `json:"username" xml:"username"`
 	Password string `json:"password" xml:"password"`
 }
+
+// struct used to define the metadata headers used
+// by the authenticators to get the username, password, etc.
+type MetadataInfo struct {
+	// header holding the username data.
+	HDRUsername string `json:"userheader" xml:"userheader"`
+	// header holding the password (or token) data.
+	HDRPassword string `json:"passheader" xml:"passheader"`
+}
