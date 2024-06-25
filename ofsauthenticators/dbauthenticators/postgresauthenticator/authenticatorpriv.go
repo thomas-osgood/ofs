0912e67df0b0c2a4010dae01195c66a7e7408bf5
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-24 23:23:50 -0400

    began execStringQuery func

--- ofsauthenticators/dbauthenticators/postgresauthenticator/authenticatorpriv.go
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/authenticatorpriv.go
@@ -1,6 +1,13 @@
 package postgresauthenticator
 
-import "context"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+
+	dbamessages "github.com/thomas-osgood/ofs/ofsauthenticators/dbauthenticators/internal/messages"
+)
 
 // function designed to ping the database and make sure the
 // connection is valid.
@@ -14,4 +21,46 @@ func (pga *PostGresAuthenticator) checkConnection() (err error) {
 	defer cancel()
 
 	return pga.db.PingContext(ctx)
+}
+
+// generic function designed to execute a given SQL query and
+// return a single two column string row.
+func (pga *PostGresAuthenticator) execStringQuery(query string, sqlargs []any) (value1 string, value2 string, err error) {
+	var cancel context.CancelFunc
+	var statement *sql.Stmt
+	var str1 sql.NullString
+	var str2 sql.NullString
+	var tx *sql.Tx
+	var txctx context.Context
+
+	txctx, cancel = context.WithTimeout(context.Background(), time.Duration(pga.timeout))
+	defer cancel()
+
+	if tx, err = pga.db.BeginTx(txctx, nil); err != nil {
+		return "", "", fmt.Errorf(dbamessages.ERR_TX_CONN, err.Error())
+	}
+
+	if statement, err = tx.Prepare(query); err != nil {
+		return "", "", fmt.Errorf(dbamessages.ERR_SQL_PREP, err.Error())
+	}
+	defer statement.Close()
+
+	err = statement.QueryRow(sqlargs...).Scan(&str1, &str2)
+	if err != nil {
+		return "", "", err
+	}
+
+	if str1.Valid {
+		value1 = str1.String
+	} else {
+		return "", "", fmt.Errorf(dbamessages.ERR_COLUMN_NULL)
+	}
+
+	if str2.Valid {
+		value2 = str2.String
+	} else {
+		return "", "", fmt.Errorf(dbamessages.ERR_COLUMN_NULL)
+	}
+
+	return value1, value2, nil
 }
