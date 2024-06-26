490a72fe9cf82bf2cb774cdf077a6a5d63e1711c
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-25 20:21:12 -0400

    added ValidateUser func

---
+++ ofsauthenticators/dbauthenticators/postgresauthenticator/authenticator.go
@@ -0,0 +1,33 @@
+package postgresauthenticator
+
+import (
+	"fmt"
+
+	pgaqueries "github.com/thomas-osgood/ofs/ofsauthenticators/dbauthenticators/postgresauthenticator/internal/queries"
+	"google.golang.org/grpc/metadata"
+)
+
+func (pga *PostGresAuthenticator) ValidateUser(md metadata.MD) (err error) {
+	var hashedpw string
+	var password string
+	var username string
+	var query string = fmt.Sprintf(
+		pgaqueries.SELECT_USER_PASSWORD,
+		pga.tableinfo.Usercolumn,
+		pga.tableinfo.Passcolumn,
+		pga.tableinfo.Tablename,
+		pga.tableinfo.Usercolumn,
+	)
+
+	username, password, err = pga.readMetadataInfo(md)
+	if err != nil {
+		return err
+	}
+
+	_, hashedpw, err = pga.execStringQuery(query, []any{username})
+	if err != nil {
+		return err
+	}
+
+	return pga.hasher.ComparePassHash([]byte(password), []byte(hashedpw))
+}
