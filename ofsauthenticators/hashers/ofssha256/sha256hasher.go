2b4c33f59aa71aec27e11d6d06d3f21f14fca92b
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-25 23:49:45 -0400

    added comment

--- ofsauthenticators/hashers/ofssha256/sha256hasher.go
+++ ofsauthenticators/hashers/ofssha256/sha256hasher.go
@@ -1,3 +1,6 @@
+// module designed to define a SHA256 hasher that can be used with
+// the Osgood File Server's Authenticators. this will fit the Hasher
+// interface defined in ofsauthenticators/interfaces.
 package ofssha256
 
 import (
