f5a2eef7b19f2b502165c669b5f706bcffc54237
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-28 18:55:28 -0400

    began microsoft authenticator object

---
+++ ofsauthenticators/microsoftauthenticators/structs.go
@@ -0,0 +1,22 @@
+package microsoftauthenticators
+
+// reference:
+//
+// https://learn.microsoft.com/en-us/entra/msal/go/
+type PublicClientAuthenticator struct {
+	authority    string
+	clientid     string
+	clientsecret string
+	scope        []string
+}
+
+type PubClientAuthOption struct {
+	// URL that will be used to verify the credentials.
+	Authority string
+	// ClientID that will be used when creating the public client.
+	Clientid string
+	// Secret that will be used when creating the public client.
+	Clientsecret string
+	// Permissions for the public client.
+	Scope []string
+}
