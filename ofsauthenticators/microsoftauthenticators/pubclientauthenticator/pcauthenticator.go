237e8fd5d47bf318cb01f5ec8afbb70f1c24108f
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-28 23:35:37 -0400

    added comment

--- ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticator.go
+++ ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticator.go
@@ -4,6 +4,10 @@ import "google.golang.org/grpc/metadata"
 
 // function designed to utilize MSAL to contact Microsoft and
 // verify the login/authorization request.
+//
+// ref:
+//
+// https://learn.microsoft.com/en-us/entra/msal/go/
 func (pca *PublicClientAuthenticator) ValidateUser(md metadata.MD) (err error) {
 	return nil
 }
