eb5bc3cf2a2470b315dc8e8e72bdd9f8f2f93aa7
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 16:41:44 -0400

    added DEFAULT_TIMEOUT

--- ofsauthenticators/microsoftauthenticators/internal/defaults/defaults.go
+++ ofsauthenticators/microsoftauthenticators/internal/defaults/defaults.go
@@ -1,4 +1,9 @@
 package defaults
 
+import "time"
+
 const DEFAULT_HEADER_PASSWORD string = "password"
 const DEFAULT_HEADER_USERNAME string = "username"
+
+// amount of time to wait until an authentication request times out.
+const DEFAULT_TIMEOUT time.Duration = 30 * time.Second
