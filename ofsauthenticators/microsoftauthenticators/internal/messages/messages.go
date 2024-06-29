b4c784d237517045540829feb8eebf71a484e294
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-28 23:32:51 -0400

    added ERR_SCOPE_NULL

--- ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
+++ ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
@@ -3,3 +3,4 @@ package messages
 const ERR_AUTHORITY_NULL string = "authority cannot be an empty string"
 const ERR_CLIENTID_NULL string = "clientid cannot be an empty string"
 const ERR_CLIENTSECRET_NULL string = "client secret cannot be an empty string"
+const ERR_SCOPE_NULL string = "scope must contain at least one entry"
