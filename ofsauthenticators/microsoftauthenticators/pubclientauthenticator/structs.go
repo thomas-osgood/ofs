9ebd47f99b889ff2caa00bdeedcabbbd8f7514af
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 16:03:34 -0400

    updated structs

--- ofsauthenticators/microsoftauthenticators/pubclientauthenticator/structs.go
+++ ofsauthenticators/microsoftauthenticators/pubclientauthenticator/structs.go
@@ -1,22 +1,20 @@
 package pubclientauthenticator
 
+import "github.com/AzureAD/microsoft-authentication-library-for-go/apps/public"
+
 // reference:
 //
 // https://learn.microsoft.com/en-us/entra/msal/go/
 type PublicClientAuthenticator struct {
-	authority    string
-	clientid     string
-	clientsecret string
-	scope        []string
+	app   public.Client
+	scope []string
 }
 
 type PubClientAuthOption struct {
-	// URL that will be used to verify the credentials.
-	Authority string
 	// ClientID that will be used when creating the public client.
 	Clientid string
 	// Secret that will be used when creating the public client.
-	Clientsecret string
+	Tenantid string
 	// Permissions for the public client.
 	Scope []string
 }
