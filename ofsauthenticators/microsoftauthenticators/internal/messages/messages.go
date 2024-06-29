b5c3d2363b6bbe6c455427fe2875d4233eb7daa3
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-28 23:30:57 -0400

    added ERR_AUTHORITY_NULL

--- ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
+++ ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
@@ -1,4 +1,5 @@
 package messages
 
+const ERR_AUTHORITY_NULL string = "authority cannot be an empty string"
 const ERR_CLIENTID_NULL string = "clientid cannot be an empty string"
 const ERR_CLIENTSECRET_NULL string = "client secret cannot be an empty string"
