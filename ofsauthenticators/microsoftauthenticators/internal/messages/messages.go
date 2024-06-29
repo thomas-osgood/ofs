546aa2b656b0609e09dd898b65023e4b530395dc
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 16:37:54 -0400

    added password and username null messages

--- ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
+++ ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
@@ -2,5 +2,7 @@ package messages
 
 const ERR_AUTHORITY_NULL string = "authority cannot be an empty string"
 const ERR_CLIENTID_NULL string = "clientid cannot be an empty string"
+const ERR_PASSWORD_NULL string = "no password or token found"
 const ERR_SCOPE_NULL string = "scope must contain at least one entry"
 const ERR_TENANTID_NULL string = "tenant id cannot be an empty string"
+const ERR_USERNAME_NULL string = "no username found"
