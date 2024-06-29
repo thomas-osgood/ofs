12c07f28cc61333fc1a7a164513fcd4ec4586e88
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 16:43:25 -0400

    added logic to ValidateUser

--- ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticator.go
+++ ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticator.go
@@ -2,7 +2,12 @@
 // can be used by OFS to verify a user.
 package pubclientauthenticator
 
-import "google.golang.org/grpc/metadata"
+import (
+	"context"
+
+	madefaults "github.com/thomas-osgood/ofs/ofsauthenticators/microsoftauthenticators/internal/defaults"
+	"google.golang.org/grpc/metadata"
+)
 
 // function designed to utilize MSAL to contact Microsoft and
 // verify the login/authorization request.
@@ -11,5 +16,23 @@ import "google.golang.org/grpc/metadata"
 //
 // https://learn.microsoft.com/en-us/entra/msal/go/
 func (pca *PublicClientAuthenticator) ValidateUser(md metadata.MD) (err error) {
+	var cancel context.CancelFunc
+	var ctx context.Context
+	var password string
+	var username string
+
+	username, password, err = pca.readMetadata(md)
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), madefaults.DEFAULT_TIMEOUT)
+	defer cancel()
+
+	_, err = pca.app.AcquireTokenByUsernamePassword(ctx, pca.scope, username, password)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
