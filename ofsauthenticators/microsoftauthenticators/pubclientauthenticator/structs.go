253738158c83176782e5326dbf29d9251f6f0fad
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 17:03:32 -0400

    comment update

--- ofsauthenticators/microsoftauthenticators/pubclientauthenticator/structs.go
+++ ofsauthenticators/microsoftauthenticators/pubclientauthenticator/structs.go
@@ -24,9 +24,6 @@ type PubClientAuthOption struct {
 
 // struct defining the header names for the username and
 // password headers that will be read from the metadata.
-//
-// note: "password" could mean a password/passphrase or
-// a token to use for authentication.
 type AuthHeaders struct {
 	HdrPassword string
 	HdrUsername string
