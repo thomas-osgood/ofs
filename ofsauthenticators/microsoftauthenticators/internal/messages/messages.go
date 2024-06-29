baa471c5308525d83c7c9dea6c5a107261773567
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 16:02:53 -0400

    updated messages

--- ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
+++ ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
@@ -2,5 +2,5 @@ package messages
 
 const ERR_AUTHORITY_NULL string = "authority cannot be an empty string"
 const ERR_CLIENTID_NULL string = "clientid cannot be an empty string"
-const ERR_CLIENTSECRET_NULL string = "client secret cannot be an empty string"
 const ERR_SCOPE_NULL string = "scope must contain at least one entry"
+const ERR_TENANTID_NULL string = "tenant id cannot be an empty string"
