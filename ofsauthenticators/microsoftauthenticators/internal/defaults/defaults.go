cfc2287661c7f09fe6549fec03f04be367687fb4
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 23:11:15 -0400

    updated const

--- ofsauthenticators/microsoftauthenticators/internal/defaults/defaults.go
+++ ofsauthenticators/microsoftauthenticators/internal/defaults/defaults.go
@@ -2,7 +2,7 @@ package defaults
 
 import "time"
 
-const DEFAULT_HEADER_PASSWORD string = "password"
+const DEFAULT_HEADER_TOKEN string = "token"
 const DEFAULT_HEADER_USERNAME string = "username"
 
 // amount of time to wait until an authentication request times out.
