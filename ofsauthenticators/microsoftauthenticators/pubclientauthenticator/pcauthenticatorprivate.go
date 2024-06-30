2b7f1c19c7c09f8e160e89f0040e57f7451cd0f7
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-29 23:13:48 -0400

    updated readMetadata logic

--- ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticatorprivate.go
+++ ofsauthenticators/microsoftauthenticators/pubclientauthenticator/pcauthenticatorprivate.go
@@ -10,17 +10,12 @@ import (
 
 // function designed to extract the username and password information
 // from the passed in metadata.
-func (pca *PublicClientAuthenticator) readMetadata(md metadata.MD) (username string, password string, err error) {
+func (pca *PublicClientAuthenticator) readMetadata(md metadata.MD) (token string, err error) {
 
-	password = strings.TrimSpace(md.Get(pca.headerInfo.HdrPassword)[0])
-	if len(password) < 1 {
-		return "", "", fmt.Errorf(mamessages.ERR_PASSWORD_NULL)
+	token = strings.TrimSpace(md.Get(pca.headerInfo.HdrToken)[0])
+	if len(token) < 1 {
+		return "", fmt.Errorf(mamessages.ERR_TOKEN_NULL)
 	}
 
-	username = strings.TrimSpace(md.Get(pca.headerInfo.HdrUsername)[0])
-	if len(username) < 1 {
-		return "", "", fmt.Errorf(mamessages.ERR_USERNAME_NULL)
-	}
-
-	return username, password, nil
+	return token, nil
 }
