9649ce7aa068ee168c5f5007f538328bcf961b96
thomas-osgood <thomas-osgood@users.noreply.github.com>
2024-06-30 14:37:30 -0400

    added ERR_JWT_INVALID

--- ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
+++ ofsauthenticators/microsoftauthenticators/internal/messages/messages.go
@@ -2,6 +2,7 @@ package messages
 
 const ERR_AUTHORITY_NULL string = "authority cannot be an empty string"
 const ERR_CLIENTID_NULL string = "clientid cannot be an empty string"
+const ERR_JWT_INVALID string = "invalid jwt"
 const ERR_SCOPE_NULL string = "scope must contain at least one entry"
 const ERR_TENANTID_NULL string = "tenant id cannot be an empty string"
 const ERR_TIMEOUT_SMALL string = "timeout must be at least one second"
